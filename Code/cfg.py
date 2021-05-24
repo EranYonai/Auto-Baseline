a278dff6e354de92073e64dddace2ea86b2d437b
Eran Yonai <71071063+EranYonai@users.noreply.github.com>
2021-05-24 13:19:20 +0300

    Updated catalog search function
    - Moved PS_PATH to config file.
    - Moved the command variable to the config file.
    - updated print command in exception
    - for some reason, I needed to install LXML library from pip, see how it affects the exe build.

--- Code/cfg.py
+++ Code/cfg.py
@@ -2,7 +2,8 @@ PASSWORDS = {
     'DB_MANAGER': 'dbManager'
 }
 FILE_PATHS = {
-    'DB_LOCATION': "C:\\Users\\eyonai\\OneDrive - JNJ\\Documents\\GitHub\\Baseliner\\Code\\db\\"
+    'DB_LOCATION': "C:\\Users\\eyonai\\OneDrive - JNJ\\Documents\\GitHub\\Baseliner\\Code\\db\\",
+    'POWERSHELL': 'C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe'
 }
 
 APPLICATION_VERSION = {
@@ -118,3 +119,20 @@ TABLE_FIELDS = {
 MIN_CORRELATION = 0.7
 MAX_CORRELATION = 1
 PERCENTAGE_TO_PASS_DB = 0.8
+
+SEARCH_CATALOG_COMMAND = '$s = Invoke-WebRequest -Uri "http://itsusrawsp10939.jnj.com/partnolookup/Main.aspx" -UseDefaultCredentials `\n' \
+                    '-Method "POST" `\n' \
+                    '-Headers @{ \n' \
+                    '"Cache-Control"="max-age=0"\n' \
+                    '"Upgrade-Insecure-Requests"="1"\n' \
+                    '"Origin"="http://itsusrawsp10939.jnj.com"\n' \
+                    '"User-Agent"="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36 Edg/86.0.622.58"\n' \
+                    '"Accept"="text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"\n' \
+                    '"Referer"="http://itsusrawsp10939.jnj.com/partnolookup/Main.aspx"\n' \
+                    '"Accept-Encoding"="gzip, deflate"\n' \
+                    '"Accept-Language"="en-US,en;q=0.9,he;q=0.8"\n' \
+                    '"Cookie"="_ga=GA1.2.231059344.1603268627; ASP.NET_SessionId=appg4m3nazairir3shlei445"\n' \
+                    '} `\n' \
+                    '-ContentType "application/x-www-form-urlencoded" `\n' \
+                    '-Body ("__EVENTTARGET=&__EVENTARGUMENT=&__VIEWSTATE=Jx1bFdZUxxK%2FIyJlVuaAS8d6GW%2F2MZKhNWzn7ZDD4xp1ptT4Zl4pr7PS1o7yb2RO%2F%2FFSGz7MkXsqu%2FD2i5c2hzHdq4FgBfjUoW5QDaa14%2BIlCGqsZUAVFmiB6TenAnicIBfJvHNjYRtz62Zag0Tq01i5ZKcngUt9DMa3d0ws7N%2BPGfciV7w3McgzH8SRRKPTKHutTYZ5SL%2FdEAQ7e0D2RpWwOpm9Z8mAtMVvuDz17wCjFqCEH37Sx3NoSW9cSOKkb8RNekuuCgE9S2%2BFaHyMs%2B3dPa9QtsdG3YcAfIZ8LwzByZRSfrWF6MfhaAfNOEWfeyfxaSWZZx%2B4amLCXwPiBimvxA%2Ff5jJUovUttuH%2FU2%2Fx3rwfGajlM4o84vrIKKl1qjsZhuTCwIn8bfz9HxieyQDjeRNhTgGuAt1MK6fGmVD2BWpaGd9w2yHTE222v7zACxQu8WrWn1SnJMZbeJyyLD3sQm91Ne57QgYRMGRycq1Ou2QGOUkF%2FuAulkjOvIFAjPe07zHCyV9kX8h49NqWuOGGgZQShSRQWJdUVaraux10Nqmc1a5auzT7JlRtobnNHDUVCsjBKykwiHawyzKJNe%2BQxWcfmLbDVSMP9JTJxvt%2Fr%2FNCyUL9sN4jcA3r8aGjjilxIsxVILKlgwDA%2FM5xO010AtJ1jsOe4U%2FvLkfngznvVUxxU3KfU55IP2X7uHeCZ8Ei%2F%2FSadtvULUvJPqzib%2F4Rwn5MhmRAn6ePcUDVaxDPzeTYAp4c3MPQm4oRH85MRRXA77lA6XmIaf8W0SaksvE8DOhSK79azfOeYiVB21osOqiQJhej3m4%2F%2FLQlTdUuP8OBeueQ56H%2ByUhU6e6q4yeVTQxeG0dsAyi%2B5AyduS2zgocfLck6hkiX2octeVY4nREgV%2Bewb8%2B%2BVMAbt1Wha5K03CvxUa%2BGPBLepjdPeWw28YUij0%2BJs5%2BoFE1MyP%2BadM2dKWPQDWM4CQgGhHYEtpEt17sumnvQA868taWFwHA88MZd%2BtXiYZMWquDmekKVYMaFsoiedfY8w897rQNb0gSLXoWXOs14JtVIYx%2BNu4%2F%2FLl3YHzMniN9FtRpSvQEmDY7AYe1luXcG%2BtZb4lo%2FVdADc6QOXXbGITK%2BECq7ww3RLrYlY3KKwBI6QkRxfMLQmznuNIdm5RmST%2BmJJ0JIUVQyQ5NTuGxx9CiQEP4TcpamMZ%2FSVbTpykWrxL44kI7X44SqbPsx26fD71Qg6WXyLXkRjx%2BuwMJjyss7Dgt288RDpiwGLkQ1ccSpx3%2BCsJyI0HEKyh38rClj8XgHYZiiszxkH9rsizQkzt0mHjPrBVkSUaZOjGN7HTzMyv3zJCqohdjYVruoRCDlWB%2F8UgfTRRzF9PBgTToojqnC5wwWNtB83XMMcK83niy9ycDAL16thx0vxhx2rVdK9GnPaC5C%2FVS6dkA8seryip8XYdKXQwuCBVwhKCUgWtWu21ffFSNcKSYh4zOylC4yJd%2FHD5I8kA%2BBP54%3D&__VIEWSTATEGENERATOR=3F1D2CC7&__VIEWSTATEENCRYPTED=&__EVENTVALIDATION=3MeVR2ARcxwwgDIpmzhIz6e0P%2BHEJIzw%2BEmhH%2Fvny6FuNhtcq9LABi52ZS5av3em90%2F1VT6RDNWHykf3qrkb9HTUg%2FC472C8gauFaFoiRfHIIejdITmRrmJyNlSymlUH8jdo%2Fw%3D%3D&txtPartNo=" + $txt + "&btnLookup=Search&TextBoxWatermarkExtender1_ClientState=&txtSRP5=&TextBoxWatermarkExtender2_ClientState=")\n' \
+                    '$s.Content' # command to open catheter catalog and search 'search_data'
